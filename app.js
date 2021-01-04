1a42838e3ff3e142cfdcc869cc214ef766fed8de
Christina Williams <christinaxtwilliams@gmail.com>
2021-01-04 16:31:13 -0500

    change

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
+
+const apiKey = "1306e81b70b046a853c00097ad4899b8";
