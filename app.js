72911d6ed0098ae9d9e9f5040994a901ed13a7a4
Christina Williams <christinaxtwilliams@gmail.com>
2021-01-05 18:52:52 -0500

    city check

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ const apiKey = "1306e81b70b046a853c00097ad4899b8";
 form.addEventListener("submit", e => {
   e.preventDefault();
   let inputVal = input.value;
+
+  //check if there's already a city
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
